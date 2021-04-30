412773258000c89d781bc35dfb6b8518e40325cd
liweicheng <liweicheng@shengqugames.com>
2021-04-30 18:55:59 +0800

    add premake

--- premake5.lua
+++ premake5.lua
@@ -24,20 +24,18 @@ project "Caxi"
 		"%{prj.name}/src/**.cpp"
 	}
 
-	include
+	includedirs
 	{
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
 	filter "system:windows"
-		cppdialect "C++20"
+		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
 
 		defines
 		{
-			"_DEBUG",
-			"_CONSOLE",
 			"CX_PLATFORM_WINDOWS",
 			"CX_BUILD_DLL"
 		}
@@ -46,3 +44,62 @@ project "Caxi"
 		{
 			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Game")
 		}
+
+	filter "configurations:Debug"
+		defines "CX_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CX_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CX_DIST"
+		optimize "On"
+
+project "Game"
+	location "Game"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Caxi/vendor/spdlog/include",
+		"Caxi/src"
+	}
+
+	links
+	{
+		"Caxi"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CX_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CX_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CX_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CX_DIST"
+		optimize "On"
